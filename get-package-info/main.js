98563f932382f17f1159f35f313979d653e848ed
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-11-08 14:04:50 -0500

    fix: debug path

--- get-package-info/main.js
+++ get-package-info/main.js
@@ -2,10 +2,14 @@ const core = require('@actions/core');
 
 const path = core.getInput('path', { required: true });
 
-const package = require(path)
+try {
+  const package = require(path)
 
-const package_version = package.version
-const package_name = package.name
+  const package_version = package.version
+  const package_name = package.name
 
-core.setOutput('package_version', package_version)
-core.setOutput('package_name', package_name)
+  core.setOutput('package_version', package_version)
+  core.setOutput('package_name', package_name)
+} catch {
+  console.log(process.cwd())
+}
