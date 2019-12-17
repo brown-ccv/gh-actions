47c28441266cf85996a371398c14aadd31ec4013
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-12-17 17:04:28 -0500

    feat: change to check for waiting instead of apply

---
+++ check-wait-label/main.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+const { getPrTime, applyLabel, removeLabel, getPrIds } = require("./utils");
+
+async function run() {
+  try {
+    const repo = context.repo;
+    const githubToken = core.getInput("GITHUB_TOKEN", {required: true});
+    const waitTime = parseInt(core.getInput("wait_time", {required: true}))
+    const waitLabel = core.getInput("wait_label")
+    const doneLabel = core.getInput("done_waiting_label")
+
+    if (!number) {
+      core.setFailed("This action only works for pull_request");
+      return;
+    }
+
+    const octokit = new GitHub(githubToken)
+
+    const prs = await getPrIds(octokit, repo, waitLabel)
+
+    for (const i in prs) {
+      let number = prs[i]
+      let prTime = await getPrTime(octokit, repo, number)
+      let nowTime = Date.now()
+      let timeWaited = (nowTime - prTime) / 24 / 60 / 60 / 1000
+
+      if (timeWaited - waitTime >= 0) {
+        await applyLabel(octokit, repo, number, doneLabel)
+        await removeLabel(octokit, repo, number, waitLabel)
+      } else {
+        await removeLabel(octokit, repo, number, doneLabel) // just in case
+        await applyLabel(octokit, repo, number, waitLabel)
+      }
+    }
+
+  } catch ({ message }) {
+    core.setFailed(message);
+  }
+}
+
+run();
