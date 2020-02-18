077348fb434566b5dac196978a003dfba49fb4cd
fernandogelin <fernando@fernandogelin.com>
2020-02-18 16:28:15 -0500

    refactor: remove input

--- combine-yml/index.js
+++ combine-yml/index.js
@@ -1,9 +1,6 @@
-const core = require("@actions/core");
 const glob = require("@actions/glob");
 
-const globOptions = {
-  followSymbolicLinks:
-    core.getInput("follow-symbolic-links").toUpper() !== "FALSE"
-};
-const globber = glob.create(core.getInput("files"), globOptions);
+const globber = glob.create("*/*.yml", {
+  followSymbolicLinks: false
+});
 globber.globGenerator().then(files => files.map(file => console.log(file)));
