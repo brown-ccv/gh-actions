d96f6068df1b1eaf294bcb79ecd19eeaa09ff2ea
fernandogelin <fernando@fernandogelin.com>
2020-02-18 18:47:22 -0500

    build:

--- combine-yml/index.js
+++ combine-yml/index.js
@@ -1,6 +1,22 @@
 const glob = require("@actions/glob");
+const core = require("@actions/core");
+const yaml = require("yaml");
+const fs = require("fs");
 
-const globber = glob.create("*/*.yml", {
-  followSymbolicLinks: false
-});
-globber.glob().then(files => files.map(file => console.log(file)));
+const patterns = ["*/*.yml"];
+const globber = glob.create(patterns.join("\n"));
+
+globber.then(glob =>
+  glob
+    .glob()
+    .then(arr => {
+      let master = { apps: [], software: [], talks: [] };
+      arr.map(path => {
+        const category = path.split("/")[6];
+        const file = fs.readFileSync(path, "utf8");
+        master[path.split("/")[6]].push(yaml.parse(file));
+      });
+      fs.writeFileSync("file.json", JSON.stringify(master));
+    })
+    .catch(error => console.log(error))
+);
