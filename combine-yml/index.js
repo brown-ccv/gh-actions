7e627c820a8b1c83b0ae43e64d3da4753e718e62
fernandogelin <fernando@fernandogelin.com>
2020-02-18 19:25:56 -0500

    test:

--- combine-yml/index.js
+++ combine-yml/index.js
@@ -7,13 +7,18 @@ const patterns = ["./apps/*.yml", "./software/*.yml", "./talks/*.yml"];
 const globber = glob.create(patterns.join("\n"));
 
 console.log(process.cwd());
+fs.readdir(process.cwd(), (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});
+
 globber.then(glob =>
   glob
     .glob()
     .then(arr => {
       let master = { apps: [], software: [], talks: [] };
       arr.map(path => {
-        console.log(path);
         const category = path.split("/")[6];
         const file = fs.readFileSync(path, "utf8");
         master[path.split("/")[6]].push(yaml.parse(file));
