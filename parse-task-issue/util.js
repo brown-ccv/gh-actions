1e67f1c3687ded2c7c1521314498f98260f6852d
Rashi Dhar <rdhar1997@gmail.com>
2020-04-30 15:39:43 -0400

    feat: add validation for the issue content

--- parse-task-issue/util.js
+++ parse-task-issue/util.js
@@ -3,11 +3,31 @@ const fs = require('fs');
 
 
 function getYMLFileContent(issue) {
-  let start = issue.body.indexOf("```")+3
+  let start = issue.body.indexOf("taskName")
   let end = issue.body.lastIndexOf("```")
   var newtask = jsyaml.load(issue.body.substring(start, end));
+  if(removeJunkAndValidateYML(contents)===null) {return null};
   let yamlStr = jsyaml.safeDump(newtask)
   return yamlStr;
+}
+
+
+function removeJunkAndValidateYML(contents){
+  if(contents.links.deployment === "https://example.com"){ contents.links.deployment = null };
+  if(contents.links.publication === "https://example.com"){ contents.links.publication = null };
+  contents.framework.library = contents.framework.library.filter(function(v) {return v.startsWith("LIBRARY")===false});
+  contents.framework.language = contents.framework.language.filter(function(v) {return v.startsWith("LANGUAGE")===false});
+  contents.lab.developers = contents.lab.developers.filter(function(v) {return v.startsWith("DEVELOPER")===false});
+  contents.tags = contents.tags.filter(function(v) {return v.startsWith("TAG")===false});
+  if(contents.taskName === null || 
+    contents.links.sourceCode === null ||
+    contents.links.sourceCode === 'https://example.com' ||
+    contents.lab.name === null ||
+    contents.lab.institution === null  
+    ){
+      return null;
+    }
+    return contents;
 }
 
 async function getIssueBody(octokit, repo, issue_number) {
