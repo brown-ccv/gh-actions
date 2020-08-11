1bb107d9768e657b8186b761daabb8146e8f52d2
Rashi Dhar <rdhar1997@gmail.com>
2020-08-11 19:15:01 -0400

    fix: 🐛 debug

--- parse-task-issue/util.js
+++ parse-task-issue/util.js
@@ -15,7 +15,7 @@ function getYMLFileContent(issue) {
 
 
 function removeJunkAndValidateYML(contents){
-  if(contents.about.deployment === "https://example.com"){ contents.about['deployment'] = null };
+  // if(contents.about.deployment === "https://example.com"){ contents.about['deployment'] = null };
   if(contents.about.publication === "https://example.com"){ contents.about.publication = null  };
   if(contents.about.sourceCode.link === 'https://github.com/example/task' ){ contents.about.sourceCode.link = null };
   contents.framework = (contents.framework!=null)?contents.framework.filter(function(v) {return v.name.startsWith("FRAMEWORK")===false}):null;
