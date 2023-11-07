6e1d6cadd568304ac378eac21d05f7b0291d7ad0
RobertGemmaJr <robert_gemma@brown.edu>
2023-11-07 12:02:44 -0500

    chore: Comments

--- get-package-info/index.js
+++ get-package-info/index.js
@@ -1,15 +1,21 @@
 const core = require('@actions/core');
-const path = require('path')
+const path = require("node:path")
+const process = require('node:process')
 
-const filePath = core.getInput('path', { required: true });
-const fullPath = path.join(process.cwd(), filePath)
+// TODO: Use a try/catch block
 
+// Get full package to package.json
+const pathInput = core.getInput('path', { required: true });
+const fullPath = path.join(process.cwd(), pathInput)
+
+// Import package and extract details
 const pkg = require(fullPath)
-
 const packageVersion = pkg.version
+// TODO: Do we need to do this replace?
 const packageName = (process.platform === 'win32')
   ? pkg.name.replace('-', '_')
   : pkg.name
 
+// Set output variables
 core.setOutput('package_version', packageVersion)
 core.setOutput('package_name', packageName)
